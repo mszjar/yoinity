bd9ec2258f5d572650efadbf13a636fd7b88e212
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-04-12 11:08:13 +0200

    validations added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
+  has_one_attached :photo
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  validates :title, :content, :url, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search, against: [ :title, :content, :url ], using: {
+    tsearch: { prefix: true }
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  has_one_attached :photo
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :nickname, presence: true
+  validates :nickname, length: { maximum: 25 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
