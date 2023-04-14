01b00f6a7d668303039410bd48a3480a48f01dba
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-04-14 15:27:16 +0200

    posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/show'
+  get 'posts/destroy'
+  get 'posts/update'
+  get 'posts/edit'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: "pages#home"
