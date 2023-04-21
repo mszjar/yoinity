6c4c56c5c29b5d0b1e9a7a2887fdfeca2797a1fd
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-04-21 14:36:19 +0200

    edit fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :photo])
   end
 
   def default_url_options
