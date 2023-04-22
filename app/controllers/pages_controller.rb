0f60a7f3f84baf5a6befe62f6f543735e774711f
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-04-22 13:53:56 +0200

    Read page and logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,6 @@ class PagesController < ApplicationController
     @post = policy_scope(Post)
     @posts = params[:query] ? Post.where("model LIKE '%#{params[:query]}%'") : Post.all
     # @posts = Post.last(3)
-    @posts = Post.last(1)
+    @posts = Post.all.sample(1)
   end
 end
