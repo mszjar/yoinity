b208e2527559c211d222267172056223fb2669fd
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-05-01 16:13:36 +0200

    controller added

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,53 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  skip_after_action :verify_authorized, only: :index
+  skip_after_action :verify_policy_scoped, only: :index
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @posts = @category.posts
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
