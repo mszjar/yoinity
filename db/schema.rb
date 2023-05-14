8c3ede2608aaca752e321f216175557362d98c58
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-05-14 19:09:47 +0200

    add country field to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_132733) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_170812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,6 +107,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_132733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "language"
+    t.string "country"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
