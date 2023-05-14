e8a8fcebb5b12420fa72db2e2df8957bff566a79
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-05-15 01:20:54 +0200

    redirection config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# redirection
+Rails.application.routes.default_url_options[:host] = 'https://www.yoinity.com'
