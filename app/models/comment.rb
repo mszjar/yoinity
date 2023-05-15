2eb1d01889f9125276974ecab42261c335987ecd
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-05-15 13:57:45 +0200

    comment_likes_ok

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :comment_replies, dependent: :destroy
+  has_many :comment_likes, dependent: :destroy
 
   validates :content, presence: true
 end
