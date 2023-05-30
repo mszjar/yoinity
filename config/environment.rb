8dce4486b69c1f09e1392068c5b54487efc49dda
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-05-30 23:01:26 +0200

    production issue

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,7 @@ require_relative "application"
 Rails.application.initialize!
 
 # Trusted hosts
-Rails.application.config.hosts << 'https://www.yoinity.com'
 Rails.application.config.hosts << 'www.yoinity.com'
-Rails.application.config.hosts << 'yoinity.com'
 
 # Redirection
-Rails.application.routes.default_url_options[:host] = 'https://www.yoinity.com'
+Rails.application.routes.default_url_options[:host] = 'www.yoinity.com'
