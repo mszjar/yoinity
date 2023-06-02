7f233c6af2a923098a187dfd9f310347f86b4758
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-06-02 07:44:37 +0200

    migrations refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_194942) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_203506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -159,6 +159,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_194942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_remixes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_remixes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_remixes_on_user_id"
   end
 
