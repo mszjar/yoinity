537022b336bfb789633ba6981b5ccf14fe8811d5
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-06-19 13:07:36 +0200

    likes ok

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -3,17 +3,20 @@ class PostLikesController < ApplicationController
 
   def create
     @post_like = current_user.post_likes.new(post_like_params)
-    if !@post_like.save
-      flash[:alert] = "You already liked this post"
-    end
     @post = Post.find_by(id: @post_like.post_id)
-    redirect_to post_path(@post)
+    if @post_like.save
+      render :create
+    else
+      flash.now[:alert] = "You already liked this post"
+      render turbo_stream: turbo_stream.replace(@post, partial: "posts/post", locals: { post: @post })
+    end
   end
 
   def destroy
     @post_like = current_user.post_likes.find(params[:id])
+    @post = @post_like.post
     @post_like.destroy
-    redirect_to post_path(@post_like.post)
+    render :destroy
   end
 
   def post_like_params
