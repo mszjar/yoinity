eb3d97d8e7b5d90252ae375a938af884ff71f56e
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-06-19 12:37:31 +0200

    added uniqueness to nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_180011) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_103651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -211,6 +211,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_180011) do
     t.string "stripe_subscription_id"
     t.string "subscription_status"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
