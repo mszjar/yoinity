26015f02a9cc3f98bf84d744b6c119197520b6ee
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2023-06-20 10:18:44 +0200

    saved for later fine

--- app/controllers/saved_for_laters_controller.rb
+++ app/controllers/saved_for_laters_controller.rb
@@ -7,18 +7,23 @@ class SavedForLatersController < ApplicationController
 
   def create
     @saved_for_later = current_user.saved_for_laters.new(saved_for_later_params)
-    if !@saved_for_later.save
-      flash[:alert] = "You already saved this story for later"
-    end
     @post = Post.find_by(id: @saved_for_later.post_id)
-    redirect_to post_path(@post)
+    if @saved_for_later.save
+      render :create
+    else
+      flash.now[:alert] = "You already saved this post for later"
+      render turbo_stream: turbo_stream.replace(@post, partial: "posts/post", locals: { post: @post })
+    end
   end
 
   def destroy
     @saved_for_later = current_user.saved_for_laters.find(params[:id])
+    @post = @saved_for_later.post
     @saved_for_later.destroy
-    redirect_to post_path(@saved_for_later.post)
+    render :destroy
   end
+
+  private
 
   def saved_for_later_params
     params.require(:saved_for_later).permit(:post_id)
